e0cf17a68d196e8b208bdea11076fefab4a8832a
harrison <851596652@qq.com>
2018-12-05 14:53:08 +0800

    6

---
+++ src/pages/rule/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="rule-page">
+    <wxParse :content="content"/>
+  </div>
+</template>
+<script>
+import wxParse from 'mpvue-wxparse'
+export default {
+  components: {
+    wxParse
+  },
+  data () {
+    return {
+      content: `<ul>
+  <li>发票打飞机萨芬</li>
+  <li>发票打飞机萨芬</li>
+  <li>发票打飞机萨芬</li>
+  <li>发票打飞机萨芬</li>
+  <li>发票打飞机萨芬</li>
+</ul>`
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+  @import "~@/common/style/mixin.styl"
+  @import "~@/common/style/color.styl"
+  @import url("~mpvue-wxparse/src/wxParse.css");
+</style>
