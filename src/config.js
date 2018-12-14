df7849d1db052880dff72a529f061b0f4deecbf4
harrison <851596652@qq.com>
2018-12-14 11:27:09 +0800

    666

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 // const mjfHost = 'http://172.10.10.105:9701'
 // const realHost = 'http://132.232.15.12:9701'
-const hsHost = 'http://228i46u234.imwork.net/service-stepmath'
-// const realHost = 'https://geek.jkwljy.com/service-stepmath'
-const host = hsHost
+// const hsHost = 'http://228i46u234.imwork.net/service-stepmath'
+const realHost = 'https://geek.jkwljy.com/service-stepmath'
+const host = realHost
 const appId = 'wx4badd3f7b610bc77'
 const appSecret = 'b947fd2584ff8ca1411719373857d20d'
 const config = {
