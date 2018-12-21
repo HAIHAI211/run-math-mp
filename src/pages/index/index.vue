f1479cfd6014fce6ad48f540ae888805b77b7835
harrison <851596652@qq.com>
2018-12-21 18:20:25 +0800

    666

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -12,7 +12,7 @@
         <div class="icon"></div>
         <span class="text">签到</span>
       </div>
-      <div class="rule">活动规则></div>
+      <navigator class="rule" url="/pages/rule/main">活动规则></navigator>
       <div class="share-btn up-down-animation"  v-if="!netError">
         <div class="icon"></div>
         <span class="text">分享即领数学币</span>
